10968bb22c0676a09e73cb1bb35cee5ff8687421
yuhrtz5183 <123590113+yuhrtz5183@users.noreply.github.com>
2025-11-17 12:41:24 -0600

    added SOS creation logic for computer player type

--- src/sprint4/ComputerPlayer.java
+++ src/sprint4/ComputerPlayer.java
@@ -25,8 +25,12 @@ public class ComputerPlayer {
         this.random = new Random();
     }
 
-    // Makes a move for the computer player (randomly selects a valid tile/letter)
+    // Makes a move for the computer player (either completing an SOS or random)
     public Move makeMove(SOSBoard board) {
+        Move sosMove = findSOSMove(board);
+        if (sosMove != null) {
+            return sosMove;
+        }
         return randomMove(board);
     }
     
@@ -47,6 +51,88 @@ public class ComputerPlayer {
             return validMoves.get(random.nextInt(validMoves.size()));
         }
         return null;
+    }
+
+    // SOS finding logic
+    private Move findSOSMove(SOSBoard board) {
+        int size = board.getSize();
+
+        // Try all empty tiles with S and O
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                if (board.isTileEmpty(row, col)) {
+                    if (completeSOS(board, row, col, SOSBoard.TileState.S)) {
+                        return new Move(row, col, SOSBoard.TileState.S);
+                    }
+                    if (completeSOS(board, row, col, SOSBoard.TileState.O)) {
+                        return new Move(row, col, SOSBoard.TileState.O);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    // Checks if tile chosen would complete an SOS
+    private boolean completeSOS(SOSBoard board, int row, int col, SOSBoard.TileState letter) {
+        if (letter == SOSBoard.TileState.S) {
+            return SOScheckS(board, row, col);
+        } else {
+            return SOScheckO(board, row, col);
+        }
+    }
+
+    // Checks if S would complete an SOS
+    private boolean SOScheckS(SOSBoard board, int row, int col) {
+        int[][] directions = {
+            {0, 1},
+            {1, 0},
+            {1, 1},
+            {1, -1}
+        };
+
+        for (int[] dir : directions) {
+            int dr = dir[0];
+            int dc = dir[1];
+
+            // Check if S is at start and end of SOS
+            if (board.isValidPosition(row + dr, col + dc) && board.isValidPosition(row + 2 * dr, col + 2 * dc)) {
+                if (board.getTile(row + dr, col + dc) == SOSBoard.TileState.O &&
+                    board.getTile(row + 2 * dr, col + 2 * dc) == SOSBoard.TileState.S) {
+                    return true;
+                }
+            }
+            if (board.isValidPosition(row - dr, col - dc) && board.isValidPosition(row - 2 * dr, col - 2 * dc)) {
+                if (board.getTile(row - dr, col - dc) == SOSBoard.TileState.O &&
+                    board.getTile(row - 2 * dr, col - 2 * dc) == SOSBoard.TileState.S) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // Checks if O would complete an SOS
+    private boolean SOScheckO(SOSBoard board, int row, int col) {
+        int[][] directions = {
+            {0, 1},
+            {1, 0},
+            {1, 1},
+            {1, -1}
+        };
+
+        for (int[] dir : directions) {
+            int dr = dir[0];
+            int dc = dir[1];
+
+            if (board.isValidPosition(row - dr, col - dc) && board.isValidPosition(row + dr, col + dc)) {
+                if (board.getTile(row - dr, col - dc) == SOSBoard.TileState.S &&
+                    board.getTile(row + dr, col + dc) == SOSBoard.TileState.S) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
 
